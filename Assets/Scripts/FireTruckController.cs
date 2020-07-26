2fc3bf4b56d0f85c2a1adcb4210b50c99d3a6551
Thorsten <thorsten@thts.de>
2020-07-26 11:29:31 +0200

    Feuerwehr und Wasserspritze lenken, Diverse Fehler behoben

---
+++ Assets/Scripts/FireTruckController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTruckController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 2.5f;
+    [SerializeField] private float _rotationSpeed = 1.5f;
+
+    [SerializeField] private Transform _waterCannon = null;
+    [SerializeField] private float _waterCannonRotationSpeed = 60.0f;
+    [SerializeField] private ParticleSystem _waterCannonParticleSystem = null;
+    [SerializeField] private Transform _cameraMountRight = null;
+    [SerializeField] private Transform _cameraMountLeft = null;
+
+    private float _horizontal = 0.0f;
+    private float _vertical = 0.0f;
+
+    private Rigidbody _rigidbody = null;
+    private ParticleSystem.EmissionModule _emmision;
+
+    private bool _isCameraLeft = false;
+
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _emmision = _waterCannonParticleSystem.emission;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        _horizontal = Input.GetAxisRaw("Horizontal");
+        _vertical = Input.GetAxisRaw("Vertical");
+
+        _waterCannon.Rotate(Vector3.up, Input.GetAxis("Mouse X") * _waterCannonRotationSpeed * Time.deltaTime);
+
+        _emmision.enabled = Input.GetButton("Fire1");
+
+        //if (Input.GetButtonDown("Fire2"))
+        //{
+        //    if (_isCameraLeft)
+        //        Camera.main.transform.SetParent(_cameraMountRight, false);
+        //    else
+        //        Camera.main.transform.SetParent(_cameraMountLeft, false);
+
+        //    _isCameraLeft = !_isCameraLeft;
+        //}
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = transform.forward * _vertical * _speed;
+        _rigidbody.angularVelocity = transform.up * _horizontal * _rotationSpeed;
+    }
+}
