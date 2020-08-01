3f0d38c2ac538c0b215d1002e6ea4501409fdf88
Thorsten <thorsten@thts.de>
2020-08-02 01:53:54 +0200

    Continue Menue und Logo hinzugefügt

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,6 +17,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject _ingameUI = null;
     [SerializeField] private GameObject _gameOverPanel = null;
     [SerializeField] private TMP_Text _gameOverPointsLabel = null;
+    [SerializeField] private GameObject _pausePanel = null;
 
 
     private BuildingController[] _buildings = null;
@@ -26,6 +27,9 @@ public class GameManager : MonoBehaviour
     private float _actualGameTime = 0.0f;
 
     private int _points = 0;
+
+    private bool _isPause = false;
+    private bool _isGameStarted = false;
 
     public BuildingController ActualBuildingOnFire
     {
@@ -49,6 +53,7 @@ public class GameManager : MonoBehaviour
         _ingameUI.SetActive(false);
         _menuePanel.SetActive(true);
         _gameOverPanel.SetActive(false);
+        _pausePanel.SetActive(false);
 
     }
 
@@ -65,12 +70,29 @@ public class GameManager : MonoBehaviour
             _gameOverPointsLabel.text = string.Format("Points: {0:D3}", _points);
 
             Cursor.lockState = CursorLockMode.None;
+
+            _isGameStarted = false;
         }
 
         _countdownLabel.text = string.Format("Time: {0:D1}:{1:D2}",
                                             Mathf.FloorToInt(_actualGameTime /60.0f),
                                             Mathf.FloorToInt(_actualGameTime % 60.0f));
 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_isPause)
+            {
+                OnContinueButtonClick();
+            }
+            else
+            {
+                _pausePanel.SetActive(true);
+                Time.timeScale = 0.0f;
+                Cursor.lockState = CursorLockMode.None;
+            }
+
+            _isPause = !_isPause;
+        }
     }
 
     private void PutNextBuildingOnFire()
@@ -117,6 +139,7 @@ public class GameManager : MonoBehaviour
         _ingameUI.SetActive(true);
 
         Cursor.lockState = CursorLockMode.Locked;
+        _isGameStarted = true;
     }
 
     public void OnExitButtonClick()
@@ -127,6 +150,13 @@ public class GameManager : MonoBehaviour
     public void OnMenueButtonClick()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void OnContinueButtonClick()
+    {
+        _pausePanel.SetActive(false);
+        Time.timeScale = 1.0f;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
 
